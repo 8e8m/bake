145a089eb8d6e98a7fc1f92baf95b0f089d20256
Emil Williams <emil@chud.cyou>
2026-03-10 03:06:59 +0000

    full automation

--- debian/debian.sh
+++ debian/debian.sh
@@ -12,4 +12,12 @@ cd bake-0~$v
 dpkg-buildpackage -us -uc
 grep 'home' bake || \
   echo "if you see this message there are no references to home in the executable"
-sudo dpkg -i ../bake_0~$v-1_amd64.deb
+./bake -s2 bake.l
+for i in bake bake-dbg ../*deb ../*xz; do gpg --yes -u emil@chud.cyou --detach-sign $i; done
+rm -rf ../export-auto
+mkdir -p ../export-auto/
+cp bake bake.sig bake-dbg bake-dbg.sig ../export-auto
+cd ..
+cp bake_0~$v-1_amd64.deb bake_0~$v-1_amd64.deb.sig \
+   bake_0~$v-1.debian.tar.xz bake_0~$v-1.debian.tar.xz.sig \
+   export-auto
